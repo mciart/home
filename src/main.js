f95b2690e5b37a6b428f1b2a375b97c53d726ab4
mciart <mchzb.com@icloud.com>
2025-12-17 02:08:28 +0800

    优化更新提醒的消息格式，增加类型和持续时间

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from "@/App.vue";
 // 引入 pinia
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { ElMessage } from "element-plus";
 // swiper
 import "swiper/scss";
 import "swiper/scss/pagination";
@@ -19,5 +20,9 @@ app.mount("#app");
 navigator.serviceWorker.addEventListener("controllerchange", () => {
   // 弹出更新提醒
   console.log("站点已更新，刷新后生效");
-  ElMessage("站点已更新，刷新后生效");
+  ElMessage({
+    message: "站点已更新，刷新后生效",
+    type: "success",
+    duration: 3000,
+  });
 });
